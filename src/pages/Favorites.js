fb7d2bf38f8b624fe5eb1ef7472599359d83d9e3
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-07-05 15:51:01 -0400

    Requisito 01 done

---
+++ src/pages/Favorites.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Album extends React.Component {
+  render() {
+    return (<div data-testid="page-favorites">Album</div>);
+  }
+}
+
+export default Album;
---
+++ src/pages/ProfileEdit.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class ProfileEdit extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">Profile Edit</div>
+    );
+  }
+}
+export default ProfileEdit;
