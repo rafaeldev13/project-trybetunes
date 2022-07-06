04647678c2ffc0ad891668683babb89e90c7fa4e
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-07-06 19:04:29 -0400

    Requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumAPI from '../services/searchAlbumsAPI';
 
 const minCharacters = 2;
 class Search extends React.Component {
@@ -7,7 +9,21 @@ class Search extends React.Component {
     super();
     this.state = {
       search: '',
+      album: [],
+      artista: '',
     };
+  }
+
+  getAlbum = async () => {
+    const { search } = this.state;
+    this.setState({
+      artista: search,
+    });
+    const data = await searchAlbumAPI(search);
+    this.setState({
+      album: data,
+      search: '',
+    });
   }
 
   handlChange = ({ target }) => {
@@ -18,7 +34,7 @@ class Search extends React.Component {
   }
 
   render() {
-    const { search } = this.state;
+    const { search, album, artista } = this.state;
 
     return (
       <div data-testid="page-search">
@@ -38,11 +54,29 @@ class Search extends React.Component {
             data-testid="search-artist-button"
             type="submit"
             disabled={ search.length < minCharacters }
+            onClick={ this.getAlbum }
           >
             Pesquisar
           </button>
         </form>
+        {album.length > 1
+        && <p>{`Resultado de álbuns de: ${artista}`}</p>}
+        {album.map((albuns) => (
+          <div key={ albuns.collectionId }>
+            <img src={ albuns.artworkUrl100 } alt="Capa do Album" />
+            <p>{albuns.collectionName}</p>
+            <p>{albuns.artistName}</p>
+            <Link
+              data-testid={ `link-to-album-${albuns.collectionId}` }
+              to={ `/album/${albuns.collectionId}` }
+            >
+              Album
+            </Link>
+          </div>
+        ))}
+        {album.length === 0 ? <p>Nenhum álbum foi encontrado</p> : ''}
       </div>
+
     );
   }
 }
