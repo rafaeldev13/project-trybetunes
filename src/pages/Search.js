51f0de90a3c98952876df3fbe75513ee27d2e9b7
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-07-05 22:05:17 -0400

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import Header from '../components/Header';
 
+const minCharacters = 2;
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+    };
+  }
+
+  handlChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { search } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <label htmlFor="search">
+            <input
+              data-testid="search-artist-input"
+              id="search-artist-input"
+              type="text"
+              name="search"
+              onChange={ this.handlChange }
+              value={ search }
+            />
+          </label>
+          <button
+            data-testid="search-artist-button"
+            type="submit"
+            disabled={ search.length < minCharacters }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
